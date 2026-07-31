62cd1e96870018e7e65dcf23149fcbe95734967c
Karthik A N <ankarthik657@gmail.com>
2026-07-31 15:55:49 +0530

    Configure Firebase credentials and update client env

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,21 +1,17 @@
-
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "authexamnotes.firebaseapp.com",
-  projectId: "authexamnotes",
-  storageBucket: "authexamnotes.firebasestorage.app",
-  messagingSenderId: "198091553006",
-  appId: "1:198091553006:web:cd719d4ee1ebf391139bff"
+  authDomain: "authexamnotes-45e80.firebaseapp.com",
+  projectId: "authexamnotes-45e80",
+  storageBucket: "authexamnotes-45e80.firebasestorage.app",
+  messagingSenderId: "905142898542",
+  appId: "1:905142898542:web:95c0d48e1be2eed3b33b7f"
 };
 
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-const auth = getAuth(app)
-
-const provider = new GoogleAuthProvider()
-
-export {auth , provider}
+export { auth, provider };
